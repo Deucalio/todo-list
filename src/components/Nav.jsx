1de867c9d114842204e67fce6648f484f83b1e2e
Deucalio <hjamad89@gmail.com>
2022-12-05 22:49:55 +0500

    fix folder strucutre

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 const Nav = () => {
   return (
     <nav className="grad absolute z-10 w-60 bg-[#E0E0E0] shadow-xl md:static md:col-span-1 md:h-auto md:w-auto">
-      <div className="flex flex-col md:flex-row items-center justify-evenly pb-7">
+      <div className="flex flex-col items-center justify-evenly pb-7 md:flex-row">
         <p className="px-2 py-2 text-left text-4xl tracking-normal text-slate-600">
           To-Do
         </p>
         <svg
-          className="lg:-ml-12 md:ml-0 w-9 text-black/50"
+          className="w-9 text-black/50 md:ml-0 lg:-ml-12"
           viewBox="0 0 24 24"
         >
           <path
@@ -16,7 +16,7 @@ const Nav = () => {
         </svg>
       </div>
 
-      <div className="my-5 flex cursor-pointer flex-wrap items-center sm:gap-1 lg:gap-4 pl-1 hover:font-semibold lg:justify-center lg:-ml-20">
+      <div className="my-5 flex cursor-pointer flex-wrap items-center pl-1 hover:font-semibold sm:gap-1  lg:w-48 lg:mx-auto lg:gap-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -34,7 +34,7 @@ const Nav = () => {
         <p className="text-lg font-bold text-slate-700">Home</p>
       </div>
 
-      <div className="flex cursor-pointer flex-wrap items-center lg:gap-4  pl-1 hover:font-semibold lg:justify-center">
+      <div className="flex cursor-pointer flex-wrap items-center pl-1 hover:font-semibold  lg:w-48 lg:mx-auto lg:gap-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -53,9 +53,9 @@ const Nav = () => {
         <p className="text-lg text-slate-700">Completed tasks</p>
       </div>
 
-      <div className="my-5 flex cursor-pointer flex-wrap items-center pl-1 hover:font-semibold lg:justify-center lg:gap-4">
+      <div className="my-5   flex cursor-pointer flex-wrap items-center pl-1  hover:font-semibold  lg:w-48 lg:mx-auto lg:gap-4">
         <svg
-          className="w-8 text-slate-800/75"
+          className="w-8 text-slate-800/75 "
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
@@ -67,6 +67,41 @@ const Nav = () => {
           />
         </svg>
         <p className="text-lg text-slate-700">Important tasks</p>
+      </div>
+
+      <div className="relative my-5   flex cursor-pointer flex-wrap items-center pl-1  hover:font-semibold  lg:w-48 lg:mx-auto lg:gap-4">
+        <svg
+          className="w-8 text-slate-800/75"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+          />
+        </svg>
+
+        <div className="flex">
+          <p className="text-lg text-slate-700 ">Projects</p>
+          <svg
+            className="absolute right-0 w-8 text-slate-800/75 transition-all hover:rotate-180"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="{1.5}"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        </div>
       </div>
     </nav>
   );
