75b915cdcb591bf22aa52c13dd18466d25500485
Deucalio <hjamad89@gmail.com>
2022-12-08 23:51:41 +0500

    add transition on new project buttom

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -68,22 +68,6 @@ const Nav = ({ setOverlay, openCustomProjectPopup }) => {
         <p className="text-lg text-slate-700">Completed tasks</p>
       </div>
 
-      <div className="my-5  flex cursor-pointer flex-wrap items-center pl-1  hover:font-semibold  lg:w-48 lg:mx-auto lg:gap-4">
-        <svg
-          className="w-8 text-slate-800/75 "
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <p className="text-lg text-slate-700">Important tasks</p>
-      </div>
-
       <div
         onClick={handleChange}
         className="relative my-5  flex cursor-pointer flex-wrap items-center pl-1  hover:font-semibold  lg:w-48 lg:mx-auto lg:gap-4"
